a4c63a9a8ddce1b3abc231b9ddfb3b7f5fd508cb
junho <sseq007@naver.com>
2023-10-23 11:25:19 +0900

    feat : springboot swagger 설정

---
+++ BE/goodnews/src/main/java/com/ssafy/goodnews/config/SwaggerConfig.java
@@ -0,0 +1,52 @@
+package com.ssafy.goodnews.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springdoc.core.SpringDocUtils;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+
+@Configuration
+public class SwaggerConfig {
+
+    static {
+        SpringDocUtils.getConfig()
+                .addAnnotationsToIgnore(AuthenticationPrincipal.class);
+    }
+
+    @Bean
+    public GroupedOpenApi allApi() {
+        return GroupedOpenApi.builder()
+                .group("all")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi memberApi() {
+        return GroupedOpenApi.builder()
+                .group("member")
+                .pathsToMatch("/api/members/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi searchApi() {
+        return GroupedOpenApi.builder()
+                .group("map")
+                .pathsToMatch("/api/map/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new Info().title("API 명세서")
+                        .description("희소식 API 명세서")
+                        .version("v0.0.1"));
+    }
+}
