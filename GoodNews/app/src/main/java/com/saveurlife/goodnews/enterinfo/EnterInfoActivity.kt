5de853b37249a113a6f5d08080b4e747e660465e
선다영 <seondy99@gmail.com>
2023-10-23 17:53:22 +0900

    feat: 추가 정보 입력 및 color 설정

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/enterinfo/EnterInfoActivity.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/enterinfo/EnterInfoActivity.kt
@@ -2,11 +2,26 @@ package com.saveurlife.goodnews.enterinfo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 import com.saveurlife.goodnews.R
 
 class EnterInfoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_enter_info)
+
+        val submitButton = findViewById<TextView>(R.id.submit_info)
+        val laterButton = findViewById<TextView>(R.id.later_info)
+
+        // 정보 등록 버튼 눌렀을 때, 이벤트
+        submitButton.setOnClickListener {
+            Toast.makeText(this, "정보 등록 버튼 클릭", Toast.LENGTH_SHORT).show()
+        }
+
+        // 다음에 등록하기 버튼 눌렀을 때, 이벤트
+        laterButton.setOnClickListener {
+            Toast.makeText(this, "다음에 등록하기 버튼 클릭", Toast.LENGTH_SHORT).show()
+        }
     }
 }
