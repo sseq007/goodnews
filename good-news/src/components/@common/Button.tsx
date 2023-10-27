02fb3f7e23e0fbe7303e5c5118e835c1657cc2a1
선다영 <seondy99@gmail.com>
2023-10-27 13:56:23 +0900

    feat: 버튼 atom 구현

--- good-news/src/components/@common/Button.tsx
+++ good-news/src/components/@common/Button.tsx
@@ -1,5 +1,6 @@
 // 버튼 컴포넌트
 import React, { ReactNode } from "react";
+import styled from "styled-components";
 
 interface ButtonProps {
   size: ButtonSize;
@@ -13,8 +14,66 @@ interface ButtonProps {
 type ButtonSize = "Large" | "Medium" | "Small";
 type ButtonColor = "Main" | "Sub" | "Safe" | "Danger" | "Undefined";
 
-const Button = ({ size, onClick, children, color, className, isActive }: ButtonProps) => {
-  return <div></div>;
+type ButtonStyle = {
+  height: string;
+  fontSize: string;
+};
+
+const ButtonStyles: Record<ButtonSize, ButtonStyle> = {
+  Large: {
+    height: "48px",
+    fontSize: "20px",
+  },
+  Medium: {
+    height: "44px",
+    fontSize: "16px",
+  },
+  Small: {
+    height: "36px",
+    fontSize: "16px",
+  },
+};
+
+const COLOR_STYLES: Record<ButtonColor, string> = {
+  Main: "#274C77",
+  Sub: "#C0D6DF",
+  Safe: "#34B476",
+  Danger: "#EF476F",
+  Undefined: "#C5C5C5",
+};
+
+const StyledButton = styled.button<ButtonProps>`
+  height: ${(props) => ButtonStyles[props.size].height};
+  font-size: ${(props) => ButtonStyles[props.size].fontSize};
+  border-radius: 8px;
+  background-color: ${(props) =>
+    props.isActive ? COLOR_STYLES[props.color] : "white"};
+  border: 1px solid ${(props) => COLOR_STYLES[props.color]};
+  color: ${(props) => (props.isActive ? "white" : COLOR_STYLES[props.color])};
+  &:hover {
+    filter: brightness(0.9);
+  }
+`;
+
+const Button = ({
+  size,
+  onClick,
+  children,
+  color,
+  className,
+  isActive,
+}: ButtonProps) => {
+  return (
+    <StyledButton
+      size={size}
+      onClick={onClick}
+      color={color}
+      className={className}
+      isActive={isActive}
+    >
+      {children}
+    </StyledButton>
+  );
 };
 
 export default Button;
--- good-news/src/pages/HomePage.tsx
+++ good-news/src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import Button from "../components/@common/Button";
+
 // 메인 페이지
 const HomePage = () => {
   return (
     <div>
       <h1>메인 페이지입니다!</h1>
+      <Button className="w-full" size="Large" color="Main">안녕하세요</Button>
     </div>
   );
 };
