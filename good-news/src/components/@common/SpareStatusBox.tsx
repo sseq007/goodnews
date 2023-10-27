2d03e95a117f224f62db5bd68a01382080513926
선다영 <seondy99@gmail.com>
2023-10-27 22:49:34 +0900

    feat: InputBox, SpareStatusBox atom 개발

--- good-news/src/components/@common/SpareStatusBox.tsx
+++ good-news/src/components/@common/SpareStatusBox.tsx
@@ -1,6 +1,49 @@
 // 상태 + 아이콘 컴포넌트 (여유혼잡)
-const SpareStatusBox = () => {
-  return (<div></div>)
+import styled from "styled-components";
+import { LuAnnoyed, LuFrown, LuSmile } from "react-icons/lu";
+
+interface SpareStatusProps {
+  keyword?: SpareKeyword;
 }
 
-export default SpareStatusBox
+type SpareKeyword = "Spare" | "Confusion" | "Undefined";
+type SpareKeywordStyle = {
+  text: string;
+  Icon: React.ComponentType;
+  color: string;
+};
+const SpareKeywordStyles: Record<SpareKeyword, SpareKeywordStyle> = {
+  Spare: {
+    text: "여유",
+    Icon: LuSmile,
+    color: "#34B476",
+  },
+  Confusion: {
+    text: "혼잡",
+    Icon: LuFrown,
+    color: "#EF476F",
+  },
+  Undefined: {
+    text: "미확인",
+    Icon: LuAnnoyed,
+    color: "#C5C5C5",
+  },
+};
+
+const StyledSpareStatusBox = styled.div<SpareStatusProps>`
+  color: ${(props) => SpareKeywordStyles[props.keyword || "Undefined"].color};
+  font-weight: bold; // 폰트 설정됐으면 해당 폰트로 변경해주기! (font-family)
+`;
+
+const SpareStatusBox = ({ keyword }: SpareStatusProps) => {
+  const { text, Icon } = SpareKeywordStyles[keyword || "Undefined"];
+
+  return (
+    <StyledSpareStatusBox keyword={keyword}>
+      <span>{text}</span>
+      <Icon />
+    </StyledSpareStatusBox>
+  );
+};
+
+export default SpareStatusBox;
--- good-news/src/pages/HomePage.tsx
+++ good-news/src/pages/HomePage.tsx
@@ -1,11 +1,8 @@
-import Button from "../components/@common/Button";
-
 // 메인 페이지
 const HomePage = () => {
   return (
     <div>
       <h1>메인 페이지입니다!</h1>
-      <Button className="w-full" size="Large" color="Main">안녕하세요</Button>
     </div>
   );
 };
