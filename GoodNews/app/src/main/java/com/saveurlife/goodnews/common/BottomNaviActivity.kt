52020e65f433a7fc0b6c6ec7a733f71744ba1bef
김나연 <nayeon0016@gmail.com>
2023-10-30 15:55:49 +0900

    feat: navigation bar UI 수정

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/common/BottomNaviActivity.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/common/BottomNaviActivity.kt
@@ -22,65 +22,65 @@ class BottomNaviActivity : AppCompatActivity() {
         binding = ActivityBottomNaviBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setFragment(TAG_HOME, HomeFragment())
+//        setFragment(TAG_HOME, HomeFragment())
 
-        binding.navigationView.setOnItemSelectedListener { item ->
-            when(item.itemId) {
-                R.id.homeFragment -> setFragment(TAG_HOME, HomeFragment())
-                R.id.mapFragment -> setFragment(TAG_MAP, MapFragment())
-                R.id.familyFragment -> setFragment(TAG_MAP, FamilyFragment())
-                R.id.myPageFragment-> setFragment(TAG_MY_PAGE, MyPageFragment())
-            }
-            true
-        }
+//        binding.navigationView.setOnItemSelectedListener { item ->
+//            when(item.itemId) {
+//                R.id.homeFragment -> setFragment(TAG_HOME, HomeFragment())
+//                R.id.mapFragment -> setFragment(TAG_MAP, MapFragment())
+//                R.id.familyFragment -> setFragment(TAG_MAP, FamilyFragment())
+//                R.id.myPageFragment-> setFragment(TAG_MY_PAGE, MyPageFragment())
+//            }
+//            true
+//        }
     }
-    private fun setFragment(tag: String, fragment: Fragment) {
-        val manager: FragmentManager = supportFragmentManager
-        val fragTransaction = manager.beginTransaction()
-
-        if (manager.findFragmentByTag(tag) == null){
-            fragTransaction.add(R.id.mainFrameLayout, fragment, tag)
-        }
-
-        val home = manager.findFragmentByTag(TAG_HOME)
-        val map = manager.findFragmentByTag(TAG_MAP)
-        val family = manager.findFragmentByTag(TAG_FAMILY)
-        val myPage = manager.findFragmentByTag(TAG_MY_PAGE)
-
-        if (home != null){
-            fragTransaction.hide(home)
-        }
-        if (map != null){
-            fragTransaction.hide(map)
-        }
-        if (family != null){
-            fragTransaction.hide(family)
-        }
-        if (myPage != null) {
-            fragTransaction.hide(myPage)
-        }
-
-        if (tag == TAG_HOME) {
-            if (home!=null){
-                fragTransaction.show(home)
-            }
-        }
-        else if (tag == TAG_MAP) {
-            if (map != null) {
-                fragTransaction.show(map)
-            }
-        }
-        else if (tag == TAG_FAMILY){
-            if (family != null){
-                fragTransaction.show(family)
-            }
-        }
-        else if (tag == TAG_MY_PAGE){
-            if (myPage != null){
-                fragTransaction.show(myPage)
-            }
-        }
-
-        fragTransaction.commitAllowingStateLoss()
-    }
+//    private fun setFragment(tag: String, fragment: Fragment) {
+//        val manager: FragmentManager = supportFragmentManager
+//        val fragTransaction = manager.beginTransaction()
+//
+//        if (manager.findFragmentByTag(tag) == null){
+//            fragTransaction.add(R.id.mainFrameLayout, fragment, tag)
+//        }
+//
+//        val home = manager.findFragmentByTag(TAG_HOME)
+//        val map = manager.findFragmentByTag(TAG_MAP)
+//        val family = manager.findFragmentByTag(TAG_FAMILY)
+//        val myPage = manager.findFragmentByTag(TAG_MY_PAGE)
+//
+//        if (home != null){
+//            fragTransaction.hide(home)
+//        }
+//        if (map != null){
+//            fragTransaction.hide(map)
+//        }
+//        if (family != null){
+//            fragTransaction.hide(family)
+//        }
+//        if (myPage != null) {
+//            fragTransaction.hide(myPage)
+//        }
+//
+//        if (tag == TAG_HOME) {
+//            if (home!=null){
+//                fragTransaction.show(home)
+//            }
+//        }
+//        else if (tag == TAG_MAP) {
+//            if (map != null) {
+//                fragTransaction.show(map)
+//            }
+//        }
+//        else if (tag == TAG_FAMILY){
+//            if (family != null){
+//                fragTransaction.show(family)
+//            }
+//        }
+//        else if (tag == TAG_MY_PAGE){
+//            if (myPage != null){
+//                fragTransaction.show(myPage)
+//            }
+//        }
+//
+//        fragTransaction.commitAllowingStateLoss()
+//    }
 }
--- GoodNews/app/src/main/java/com/saveurlife/goodnews/main/MainActivity.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/main/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.saveurlife.goodnews.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
 import com.saveurlife.goodnews.R
 import com.saveurlife.goodnews.databinding.ActivityMainBinding
+import com.saveurlife.goodnews.family.FamilyFragment
+import com.saveurlife.goodnews.map.MapFragment
+import com.saveurlife.goodnews.mypage.MyPageFragment
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -23,6 +30,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         //가족 등록 모달창
         val dialog = FamilyAlarmFragment()
         dialog.show(supportFragmentManager, "FamilyAlarmFragment")
@@ -30,6 +38,21 @@ class MainActivity : AppCompatActivity() {
 
         //상단바 모달창
         setSupportActionBar(binding.toolbar)
+
+
+//        replaceFragment(HomeFragment())
+        binding.navigationView.background = null
+        binding.navigationView.menu.getItem(2).isEnabled = false
+//        binding.navigationView.setOnItemSelectedListener { item ->
+//            when (item.itemId) {
+//                R.id.homeFragment -> replaceFragment(HomeFragment())
+//                R.id.mapFragment -> replaceFragment(MapFragment())
+//                R.id.familyFragment -> replaceFragment(FamilyFragment())
+//                R.id.myPageFragment -> replaceFragment(MyPageFragment())
+//            }
+//            true
+//        }
+
 
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
@@ -41,7 +64,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.mapFragment,
                 R.id.familyFragment,
                 R.id.myPageFragment,
-                R.id.flashLightFragment
+//                R.id.flashLightFragment
             )
         )
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
@@ -52,6 +75,12 @@ class MainActivity : AppCompatActivity() {
 //        binding.myStatusUpdateButtom.setOnClickListener{
 //
 //        }
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager: FragmentManager = supportFragmentManager
+        val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+        fragmentTransaction.commit()
     }
 
     //toolbar 보여주기
