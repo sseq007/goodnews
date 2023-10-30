e244553c7d56109cebeadc75cc76324d432538a6
선다영 <seondy99@gmail.com>
2023-10-30 17:46:54 +0900

    feat:손전등 UI 수정 및 가족 페이지

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/flashlight/FlashlightFragment.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/flashlight/FlashlightFragment.kt
@@ -22,12 +22,12 @@ class FlashlightFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // RecyclerView 손전등 list
+        // RecyclerView 손전등 테스트 데이터
         var recordTestData = arrayListOf(
-            FlashlightData(FlashType.SELF, "Record 1", "... --- ..."),  // SOS 모스 부호
-            FlashlightData(FlashType.OTHER, "Record 2", "-- .- .. -."), // 예시 모스 부호
-            FlashlightData(FlashType.SELF, "Record 3", ".- .-. ."),    // 예시 모스 부호
-            FlashlightData(FlashType.SELF, "Record 4", ".- .-. .")    // 예시 모스 부호
+            FlashlightData(FlashType.SELF, "Record 1", "... --- ..."),
+            FlashlightData(FlashType.OTHER, "Record 2", "-- .- .. -."),
+            FlashlightData(FlashType.SELF, "Record 3", ".- .-. ."),
+            FlashlightData(FlashType.SELF, "Record 4", ".- .-. .")
         )
         var flashListManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         var flashListAdapter = FlashlightListAdapter(recordTestData)
