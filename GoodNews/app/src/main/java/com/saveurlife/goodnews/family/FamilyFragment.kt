31d14fc52b171bf7e57fb6b96ff7d27abd1a334e
선다영 <seondy99@gmail.com>
2023-10-31 13:09:50 +0900

    feat: 가족 ui 수정

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/family/FamilyFragment.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/family/FamilyFragment.kt
@@ -5,19 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.saveurlife.goodnews.R
+import com.saveurlife.goodnews.flashlight.FlashType
+import com.saveurlife.goodnews.flashlight.FlashlightData
 
-/**
- * A simple [Fragment] subclass.
- * Use the [FamilyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FamilyFragment : Fragment() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+
+    private lateinit var familyListRecyclerView: RecyclerView
+    private var dataListAdapter = listOf<FamilyData>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,5 +22,22 @@ class FamilyFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_family, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var testData = listOf(
+            FamilyData("김싸피", Status.HEALTHY, "2023-10-31 14:00:00"),
+            FamilyData("이싸피", Status.INJURED, "2023-10-31 13:00:00"),
+            FamilyData("싸피이", Status.DECEASED, "2023-10-30 15:00:00"),
+            FamilyData("싸피싸피", Status.NOT_SHOWN, "2023-10-29 10:00:00")
+        )
+
+        familyListRecyclerView = view.findViewById(R.id.familyList)
+        familyListRecyclerView.layoutManager = LinearLayoutManager(context)
+
+        val adapter = FamilyListAdapter(testData)
+        familyListRecyclerView.adapter = adapter
     }
 }
