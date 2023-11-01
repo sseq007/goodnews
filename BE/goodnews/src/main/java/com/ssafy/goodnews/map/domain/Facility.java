70fe53cc727bf4108a2afddf7511e537f8eae876
junho <sseq007@naver.com>
2023-11-01 10:48:59 +0900

    fix : Facility 생성자 수정

--- BE/goodnews/src/main/java/com/ssafy/goodnews/map/domain/Facility.java
+++ BE/goodnews/src/main/java/com/ssafy/goodnews/map/domain/Facility.java
@@ -9,4 +9,11 @@ public class Facility {
 
     private double 시설면적;
     private String 대피소종류;
+
+    public Facility(String input) {
+        // 예: input = "100.5,공공대피소"
+        String[] parts = input.split(",");
+        this.시설면적 = Double.parseDouble(parts[0]);
+        this.대피소종류 = parts[1];
+    }
 }
