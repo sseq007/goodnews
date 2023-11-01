b2d0965ac487c84c45689e87cfa8da76395cf9be
junho <sseq007@naver.com>
2023-11-01 10:55:04 +0900

    fix : Facility field 어노테이션 수정

--- BE/goodnews/src/main/java/com/ssafy/goodnews/map/domain/Facility.java
+++ BE/goodnews/src/main/java/com/ssafy/goodnews/map/domain/Facility.java
@@ -2,18 +2,16 @@ package com.ssafy.goodnews.map.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 @Getter
 @NoArgsConstructor
 public class Facility {
 
+    @Field("시설면적")
     private double 시설면적;
+
+    @Field("대피소종류")
     private String 대피소종류;
 
-    public Facility(String input) {
-        // 예: input = "100.5,공공대피소"
-        String[] parts = input.split(",");
-        this.시설면적 = Double.parseDouble(parts[0]);
-        this.대피소종류 = parts[1];
-    }
 }
