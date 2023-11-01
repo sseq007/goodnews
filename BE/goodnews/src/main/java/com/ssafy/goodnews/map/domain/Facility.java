8f9beec80c30ea767d1ee83db5aaa2db78f2b0c1
junho <sseq007@naver.com>
2023-11-01 11:05:26 +0900

    fix : Facility Document 어노테이션 추가

--- BE/goodnews/src/main/java/com/ssafy/goodnews/map/domain/Facility.java
+++ BE/goodnews/src/main/java/com/ssafy/goodnews/map/domain/Facility.java
@@ -2,16 +2,14 @@ package com.ssafy.goodnews.map.domain;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Field;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Getter
 @NoArgsConstructor
+@Document(collection = "goodnews")
 public class Facility {
-
-    @Field("시설면적")
     private double 시설면적;
-
-    @Field("대피소종류")
     private String 대피소종류;
 
+    // ... getters, setters, etc.
 }
