ddbfd07fd5dfea62c1d38d4b2b47c7e7a2b99028
김나연 <nayeon0016@gmail.com>
2023-11-02 20:37:03 +0900

    feat: 마이페이지 정보 수정 생년월일 dialog 변경

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/mypage/MyPageFragment.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/mypage/MyPageFragment.kt
@@ -87,7 +87,8 @@ class MyPageFragment : Fragment() {
         val calendar = dialog.findViewById<View>(R.id.calendar)
         calendar?.setOnClickListener {
             // 두 번째 다이얼로그 표시 함수 호출
-            selectDate()
+//            selectDate()
+            showBirthSettingDialog(dialogView)
         }
 
         //성별 수정 시
@@ -128,6 +129,75 @@ class MyPageFragment : Fragment() {
         }
 
 
+    }
+
+    //생년월일 변경하기
+    private fun showBirthSettingDialog(dialog: View) {
+        val year = arrayOf("1920", "1921", "1922", "1923", "1924", "1925", "1926", "1927",
+            "1928", "1929", "1930", "1931", "1932", "1933", "1934", "1935", "1936", "1937",
+            "1938", "1939", "1940", "1941", "1942", "1943", "1944", "1945", "1946", "1947",
+            "1948", "1949", "1950", "1951", "1952", "1953", "1954", "1955", "1956", "1957",
+            "1958", "1959", "1960", "1961", "1962", "1963", "1964", "1965", "1966", "1967",
+            "1968", "1969", "1970", "1971", "1972", "1973", "1974", "1975", "1976", "1977",
+            "1978", "1979", "1980", "1981", "1982", "1983", "1984", "1985", "1986", "1987",
+            "1988", "1989", "1990", "1991", "1992", "1993", "1994", "1995", "1996", "1997",
+            "1998", "1999", "2000", "2001", "2002", "2003", "2004", "2005", "2006", "2007",
+            "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016", "2017",
+            "2018", "2019", "2020", "2021", "2022", "2023")
+        val month = arrayOf("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12")
+        val day = arrayOf("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12","13", "14", "15",
+            "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31")
+
+        //dialog로 띄울 xml파일
+        val birthDialogView = LayoutInflater.from(context).inflate(R.layout.dialog_calendar_setting, null)
+
+        val yearPicker = birthDialogView.findViewById<NumberPicker>(R.id.yearPicker) //년도 picker
+        val monthPicker = birthDialogView.findViewById<NumberPicker>(R.id.monthPicker) //월 picker
+        val dayPicker = birthDialogView.findViewById<NumberPicker>(R.id.dayPicker) //일 picker
+
+        var dialogYearEdit = dialog.findViewById<TextView>(R.id.dialogMypageYear) //년도 변경 text
+        var dialogMonthEdit = dialog.findViewById<TextView>(R.id.dialogMypageMonth) //월 변경 text
+        var dialogDayEdit = dialog.findViewById<TextView>(R.id.dialogMypageDay) //일 변경 text
+
+        var requestBirth = birthDialogView.findViewById<TextView>(R.id.requestBirth) //수정 버튼
+
+        yearPicker?.minValue = 0 //최소값
+        yearPicker?.maxValue = year.size - 1 //최대값
+        yearPicker?.displayedValues = year //년도 배열에 대한 값
+        yearPicker?.wrapSelectorWheel = false
+        yearPicker?.setOnValueChangedListener { picker, oldVal, newVal ->
+            dialogYearEdit.text = "${year[newVal]}년" //선택한 값을 변경 text에 넣기
+        }
+
+        monthPicker?.minValue = 0
+        monthPicker?.maxValue = month.size - 1
+        monthPicker?.displayedValues = month
+        monthPicker?.wrapSelectorWheel = false
+        monthPicker?.setOnValueChangedListener { picker, oldVal, newVal ->
+            dialogMonthEdit.text = "${month[newVal]}월"
+        }
+
+        dayPicker?.minValue = 0
+        dayPicker?.maxValue = day.size - 1
+        dayPicker?.displayedValues = day
+        dayPicker?.wrapSelectorWheel = false
+        dayPicker?.setOnValueChangedListener { picker, oldVal, newVal ->
+            dialogDayEdit.text = "${day[newVal]}일"
+        }
+
+        //Dialog 띄우기
+        val birthDialog = AlertDialog.Builder(requireContext())
+            .setView(birthDialogView)
+            .create()
+
+        //배경 투명하게 만들기
+        birthDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        //birthDialog 보여주기
+        birthDialog.show()
+        //완료버튼 누르면 birthDialog 창 닫기
+        requestBirth.setOnClickListener{
+            birthDialog.dismiss()
+        }
     }
 
     //혈액형 변경하기
