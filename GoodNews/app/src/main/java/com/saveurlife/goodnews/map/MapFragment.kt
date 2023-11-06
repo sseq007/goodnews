36024086aed86657d677582d107fa4fb460b20de
이은경 <whddlto2782@gmail.com>
2023-11-06 09:34:09 +0900

    feat: 특정 위치에 마커 찍는 기능

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/map/MapFragment.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/map/MapFragment.kt
@@ -7,7 +7,6 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import com.saveurlife.goodnews.GoodNewsApplication
 import com.saveurlife.goodnews.R
@@ -31,33 +30,50 @@ class MapFragment : Fragment() {
 
     private lateinit var mapView: MapView
     private lateinit var mapProvider: MapTileProviderArray
-    private val REQUEST_PERMISSION_REQDUSET_CODE = 1
+
     private val mapTileArchivePath = "offline_daejeon2.sqlite" // 지도 파일 변경 시 수정1
+
+    // 스크롤 가능 범위: 한국의 위경도 범위
     val max = GeoPoint(38.6111, 131.8696)
     val min = GeoPoint(33.1120, 124.6100)
     val box = BoundingBox(max.latitude, max.longitude, min.latitude, min.longitude)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_map, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
         mapView = view.findViewById(R.id.map) as MapView
 
         val context = requireContext()
         Configuration.getInstance().load(context, GoodNewsApplication.preferences.preferences)
 
+        // 현재 기기에서 보이는 지도의 중심 좌표
+        val projection = mapView.projection
+        val centerGeoPoint = projection.fromPixels(mapView.width / 2, mapView.height / 2)
+        val centerLat = centerGeoPoint.latitude
+        val centerLon = centerGeoPoint.longitude
+
+        // 현재 내 위치(나중에 자체 트래킹할 예정)
+        var currGeoPoint = GeoPoint(36.37610711596839, 127.3952801221493)
+
         val tileSource = XYTileSource(
-            "4uMaps", // 지도 파일 변경 시 수정2 (Mapnik: OSM에서 가져온 거 또는 4uMaps: MOBAC에서 가져온 거 => sqlite 파일의 provider 값)
+            "4uMaps", // 지도 파일 변경 시 수정2 (Mapnik: OSM에서 가져온 거 또는 4uMaps: MOBAC에서 가져온 거 // => sqlite 파일의 provider 값)
             12, 15, 256, ".png",
             arrayOf("http://127.0.0.1")
         )
         val simpleReceiver = SimpleRegisterReceiver(context)
 
         try {
-            val archives = arrayOf<IArchiveFile>(ArchiveFileFactory.getArchiveFile(getMapsFile(context)))
+            val archives =
+                arrayOf<IArchiveFile>(ArchiveFileFactory.getArchiveFile(getMapsFile(context)))
             val moduleProvider = MapTileFileArchiveProvider(simpleReceiver, tileSource, archives)
             mapProvider = MapTileProviderArray(tileSource, null, arrayOf(moduleProvider)).apply {
                 setUseDataConnection(false)
@@ -74,7 +90,9 @@ class MapFragment : Fragment() {
 
 
         mapView.apply {
+
             mapView.overlays.add(tilesOverlay)
+
             mapView.zoomController.setZoomInEnabled(true)
             mapView.zoomController.setZoomOutEnabled(true)
 
@@ -101,43 +119,19 @@ class MapFragment : Fragment() {
 
             // 기기 화면 DPI에 따라 스케일 DPI 적용(기기별로 보이는 지도 크기 최대한 유사하도록)
             mapView.isTilesScaledToDpi = false
+
+            // 현재 내 위치 표시하기
+            val markerOverlay = MarkerOverlay(currGeoPoint)
+            mapView.overlays.add(markerOverlay)
+            mapView.invalidate() // 지도 다시 그려서 오버레이 보이게 함
         }
 
-    }
-
-    override fun onResume() {
-        super.onResume()
-        mapView.onResume()
-    }
-
-    override fun onPause() {
-        super.onPause()
-        mapView.onPause()
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        var permissionsToRequest = ArrayList<String>()
-        var i = 0
-        while (i < grantResults.size) {
-            permissionsToRequest.add(permissions[i])
-            i++
-        }
-        if (permissionsToRequest.size > 0) {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                permissionsToRequest.toTypedArray(),
-                REQUEST_PERMISSION_REQDUSET_CODE
-            )
-        }
     }
 
     @Throws(IOException::class)
     private fun getMapsFile(context: Context): File {
-        val resourceInputStream = context.resources.openRawResource(R.raw.offline_daejeon2) // 지도 파일 변경 시 수정3
+        val resourceInputStream =
+            context.resources.openRawResource(R.raw.offline_daejeon2) // 지도 파일 변경 시 수정3
 
         // 파일 경로
         val file = File(context.filesDir, mapTileArchivePath)
@@ -154,7 +148,19 @@ class MapFragment : Fragment() {
                 }
             }
         }
-
         return file
     }
+
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+
 }
