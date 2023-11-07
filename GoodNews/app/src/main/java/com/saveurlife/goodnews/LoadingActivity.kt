2e0d7d143d95afb2d1b1bfd5cf9378798474068b
김나연 <nayeon0016@gmail.com>
2023-11-07 15:23:33 +0900

    feat: 내 정보 수정 realm 저장 및 업데이트

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/LoadingActivity.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/LoadingActivity.kt
@@ -3,6 +3,7 @@ package com.saveurlife.goodnews
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import com.saveurlife.goodnews.databinding.ActivityLoadingBinding
 import com.saveurlife.goodnews.tutorial.TutorialActivity
 
@@ -14,9 +15,20 @@ class LoadingActivity : AppCompatActivity() {
         binding = ActivityLoadingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.start.setOnClickListener {
-            val intent = Intent(this, TutorialActivity::class.java)
-            startActivity(intent)
-        }
+        Handler().postDelayed(Runnable {
+            // 앱의 MainActivity로 넘어가기
+            val i = Intent(this@LoadingActivity, TutorialActivity::class.java)
+            startActivity(i)
+            // 현재 액티비티 닫기
+            finish()
+        }, 3000)
+
+
+
+
+//        binding.start.setOnClickListener {
+//            val intent = Intent(this, TutorialActivity::class.java)
+//            startActivity(intent)
+//        }
     }
 }
