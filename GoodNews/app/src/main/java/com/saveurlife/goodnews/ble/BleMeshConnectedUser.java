da9c37dab1a3edd6be6fe08edfd74ce9d4ce82f9
김나연 <nayeon0016@gmail.com>
2023-11-08 17:00:08 +0900

    feat: ble로 연결된 사용자들 띄우는 리스트 UI 개발

---
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/ble/BleMeshConnectedUser.java
@@ -0,0 +1,77 @@
+package com.saveurlife.goodnews.ble;
+
+
+public class BleMeshConnectedUser {
+    private String userId;
+    private String userName;
+    private String updateTime;
+    private String healthStatus;
+    private double lat;
+    private double lon;
+
+    public BleMeshConnectedUser(String userId, String userName, String updateTime, String healthStatus, double lat, double lon) {
+        this.userId = userId;
+        this.userName = userName;
+        this.updateTime = updateTime;
+        this.healthStatus = healthStatus;
+        this.lat = lat;
+        this.lon = lon;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getHealthStatus() {
+        return healthStatus;
+    }
+
+    public void setHealthStatus(String healthStatus) {
+        this.healthStatus = healthStatus;
+    }
+
+    public double getLat() {
+        return lat;
+    }
+
+    public void setLat(double lat) {
+        this.lat = lat;
+    }
+
+    public double getLon() {
+        return lon;
+    }
+
+    public void setLon(double lon) {
+        this.lon = lon;
+    }
+
+    @Override
+    public String toString() {
+        return userId + '/' + userName + '/' + updateTime + '/' + healthStatus + '/' + lat + '/' + lon;
+    }
+
+    public String toInitString(){
+        return userId + '-' + userName + '-' + updateTime + '-' + healthStatus + '-' + lat + '-' + lon;
+    }
+}
--- GoodNews/app/src/main/java/com/saveurlife/goodnews/main/MainAdapter.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/main/MainAdapter.kt
@@ -1,18 +1,18 @@
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentManager
-import androidx.fragment.app.FragmentStatePagerAdapter
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.saveurlife.goodnews.main.MainAroundListFragment
+import com.saveurlife.goodnews.main.MainAroundAdvertiseFragment
 import com.saveurlife.goodnews.main.MainFamilyAroundListFragment
 
 class MainAdapter(fragment: Fragment) :FragmentStateAdapter(fragment) {
 
-    override fun getItemCount(): Int = 2
+    override fun getItemCount(): Int = 3
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> MainAroundListFragment()
-            1 -> MainFamilyAroundListFragment()
+            0 -> MainAroundAdvertiseFragment()
+            1 -> MainAroundListFragment()
+            2 -> MainFamilyAroundListFragment()
             else -> throw IllegalStateException("Invalid position $position")
         }
     }
