0791b73f5ee12c7e6228f26c56df6ea84f99dbc4
김예진 <kky156@naver.com>
2023-11-09 14:36:32 +0900

    remove: 지도 기능 제거

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/api/MapInterface.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/api/MapInterface.kt
@@ -10,8 +10,4 @@ interface MapInterface {
     // 지도 시설 상태 등록
     @POST("map/registfacility")
     fun registMapFacility(@Body placeStateInfo: RequestBody): Call<ResponseFacilityRegist>
-
-    // 지도 시설 상세 보기
-    @POST("map/facilitydetail")
-    fun findDetailFacilityInfo(@Body facilityId: RequestBody): Call<ResponseFacilityDetail>
 }
