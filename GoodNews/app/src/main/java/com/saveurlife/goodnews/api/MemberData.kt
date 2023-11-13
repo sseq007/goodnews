ffd4e926f77d9be44d6909d50fa9792c8664483e
김예진 <kky156@naver.com>
2023-11-13 12:44:38 +0900

    feat: Map,Member api 수정
    Former-commit-id: 56f47144cc3a514cd0e798ad8cfbd697e3cf9c62

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/api/MemberData.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/api/MemberData.kt
@@ -3,11 +3,15 @@ package com.saveurlife.goodnews.api
 
 // request
 data class RequestMemberId(val memberId:String)
+data class RequestState(val state:String)
+data class RequestLocation(val lat: Double, val lon:Double)
 data class RequestMemberInfo(val name:String, val birthDate:String, val bloodType:String, val addInfo:String)
 data class RequestMemberAddInfo(val memberId:String, val name:String, val birthDate:String, val gender:String, val bloodType:String, val addInfo:String)
 
 // response
 data class ResponseLogin(val success: Boolean, val message: String, val data:FirstLogin)
+data class ResponseState(val success: Boolean, val message: String, val data:Map<String, Any>)
+data class ResponseLocation(val success: Boolean, val message: String, val data: Map<String, Any>)
 data class ResponseModifyMember(val success: Boolean, val message: String, val data:MemberModifyInfo)
 data class ResponseMember(val success:Boolean, val message:String, val data:MemberInfo)
 data class ResponseRegistMember(val success: Boolean, val message: String, val data: MemberRegistInfo)
