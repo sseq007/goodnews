49810dee1b2c7c123c2fe165e684363883073e08
김예진 <kky156@naver.com>
2023-11-13 10:13:51 +0900

    feat : server Sync

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/models/MapInstantInfo.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/models/MapInstantInfo.kt
@@ -5,13 +5,13 @@ import io.realm.kotlin.types.RealmObject
 
 class MapInstantInfo():RealmObject {
     var id:Long=0 //사용자 전화번호 위치 정보 현재 시각 정보 조합
-    var state:Char='0' // 위험 안전에 따라 한 글자로 표현
+    var state:String="" // 위험 안전에 따라 한 글자로 표현
     var content: String=""
     var time: RealmInstant= RealmInstant.from(0,0)
     var latitude: Double = 0.0
     var longitude: Double = 0.0
 
-    constructor(id: Long, state:Char, content:String, time:RealmInstant, latitude:Double, longitude:Double):this(){
+    constructor(id: Long, state:String, content:String, time:RealmInstant, latitude:Double, longitude:Double):this(){
         this.id = id
         this.state = state
         this.content = content
