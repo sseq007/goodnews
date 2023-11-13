fb0ee018a915899fd18ecbbb4ebfe5d517d55beb
김예진 <kky156@naver.com>
2023-11-13 16:38:49 +0900

    feat : member api 기능 추가 및 변경

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/api/MemberData.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/api/MemberData.kt
@@ -2,22 +2,23 @@ package com.saveurlife.goodnews.api
 
 
 // request
-data class RequestMemberId(val memberId:String)
+data class RequestMemberInfo(val name:String, val gender: String, val birthDate:String, val bloodType:String, val addInfo:String, val lat:Double, val lon: Double)
 data class RequestState(val state:String)
 data class RequestLocation(val lat: Double, val lon:Double)
-data class RequestMemberInfo(val name:String, val birthDate:String, val bloodType:String, val addInfo:String)
-data class RequestMemberAddInfo(val memberId:String, val name:String, val birthDate:String, val gender:String, val bloodType:String, val addInfo:String)
+data class RequestMemberId(val memberId:String)
+data class RequestMemberAddInfo(val memberId:String, val phoneNumber:String, val name:String, val birthDate:String, val gender:String, val bloodType:String, val addInfo:String)
 
 // response
-data class ResponseLogin(val success: Boolean, val message: String, val data:FirstLogin)
+data class ResponseModifyMember(val success: Boolean, val message: String, val data: Map<String, Any>)
 data class ResponseState(val success: Boolean, val message: String, val data:Map<String, Any>)
 data class ResponseLocation(val success: Boolean, val message: String, val data: Map<String, Any>)
-data class ResponseModifyMember(val success: Boolean, val message: String, val data:MemberModifyInfo)
 data class ResponseMember(val success:Boolean, val message:String, val data:MemberInfo)
 data class ResponseRegistMember(val success: Boolean, val message: String, val data: MemberRegistInfo)
-data class MemberInfo(val memberId:String, val name:String, val birthDate: String, val gender: String, val bloodType: String, val addInfo: String, val state:String, val lat:Double, val lon:String)
-data class MemberRegistInfo(val memberId: String, val name: String, val birthDate: String, val gender: String, val bloodType: String, val addInfo: String)
-data class MemberModifyInfo(val name: String, val birthDate: String, val bloodType: String, val addInfo: String)
+data class ResponseLogin(val success: Boolean, val message: String, val data:FirstLogin)
+
+
+data class MemberInfo(val memberId:String, val phoneNumber:String, val name:String, val birthDate: String, val gender: String, val bloodType: String, val addInfo: String, val state:String, val lat:Double, val lon:String, val familyId: String)
+data class MemberRegistInfo(val lastUpdate:String, val memberId: String, val phoneNumber: String, val name: String, val birthDate: String, val gender: String, val bloodType: String, val addInfo: String, val state:String, val lat:Double, val lon:String, val password:String, val locationTime:String, val role:String, val lastConnection:String)
 data class FirstLogin(val firstLogin: Boolean)
 
 
