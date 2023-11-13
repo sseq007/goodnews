ff4d0b79f0efa80e33819e3493b7b6e037102df2
김예진 <kky156@naver.com>
2023-11-13 17:11:12 +0900

    feat : family api 기능 추가 및 수정

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/api/FamilyData.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/api/FamilyData.kt
@@ -1,11 +1,11 @@
 package com.saveurlife.goodnews.api
 
 // request
-data class RequestPlaceId(val placeId:Int)
 data class RequestPlaceCanUse(val canuse:Boolean)
 data class RequestPlaceInfo(val name:String, val lat:Double, val lon:Double)
-data class RequestPlaceDetailInfo(val memberId:String, val name:String, val lat:Double, val lon:Double)
+data class RequestPlaceId(val placeId:Int)
 data class RequestFamilyRegist(val memberId:String, val familyId:String)
+data class RequestPlaceDetailInfo(val memberId:String, val name:String, val lat:Double, val lon:Double)
 
 // response
 data class ResponsePlaceUseUpdate(val success:Boolean, val message:String, val data:Map<String, Any>)
@@ -17,10 +17,10 @@ data class ResponsePlaceDetail(val success:Boolean, val message:String, val data
 data class ResponseMemberInfo(val success:Boolean, val message:String, val data:ArrayList<FamilyInfo>)
 data class ResponsePlaceInfo(val success:Boolean, val message:String, val data:ArrayList<PlaceInfo>)
 
-data class PlaceDetailInfo(val placeId: Int, val name:String, val lat:Double, val lon:Double, val canuse:Boolean)
+data class PlaceDetailInfo(val placeId:Int, val name:String, val lat:Double, val lon:Double, val canuse:Boolean)
 data class PlaceInfo(val placeId: Int, val name: String, val canuse: Boolean)
 data class FamilyId(val familyId: String)
-data class FamilyInfo(val memberId: String, val phoneNumber: Number, val name: String, val lastConnection:String, val state:Int, val familyId: String)
+data class FamilyInfo(val memberId: String, val phoneNumber: String, val name: String, val lastConnection:String, val state:String, val familyId: String)
 
 
 
