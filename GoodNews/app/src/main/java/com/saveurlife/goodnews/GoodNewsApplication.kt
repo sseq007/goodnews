ff8c4ddd689a6c7a1487dca63d60fb3c0cc8588d
이은경 <whddlto2782@gmail.com>
2023-11-16 01:11:51 +0900

    feat: 연결된 이용자 클릭 시 다이얼로그(data 포함) 띄우는 로직

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/GoodNewsApplication.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/GoodNewsApplication.kt
@@ -78,8 +78,8 @@ class GoodNewsApplication : Application(), Application.ActivityLifecycleCallback
 
         //오프라인 지도 위 시설정보 초기 입력
         val csvReader =
-            CSVReader(InputStreamReader(resources.openRawResource(R.raw.offmapfacilitydata)))
-//            CSVReader(InputStreamReader(resources.openRawResource(R.raw.testfacilitydata)))
+//            CSVReader(InputStreamReader(resources.openRawResource(R.raw.offmapfacilitydata)))
+            CSVReader(InputStreamReader(resources.openRawResource(R.raw.offmapfacility_wo_phar)))
         csvReader.readNext()  // 헤더 레코드를 읽고 무시
         val records = csvReader.readAll()
 
