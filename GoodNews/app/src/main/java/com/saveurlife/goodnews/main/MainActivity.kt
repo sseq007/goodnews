3c32199455f9b661229ac587c32793e14feb8082
김나연 <nayeon0016@gmail.com>
2023-11-15 13:43:29 +0900

    fix: merge 오류 수정

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/main/MainActivity.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/main/MainActivity.kt
@@ -269,12 +269,6 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        if (isBound) {
-            unbindService(connection)
-        }
-    }
 
     // LiveData 객체를 프래그먼트에서 관찰할 수 있도록 공개 메서드로 제공
 //    fun getDeviceArrayListNameLiveData(): LiveData<List<String>>? {
@@ -494,6 +488,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
+        if (isBound) {
+            unbindService(connection)
+        }
 
         //위치 정보 저장 중지
         val serviceIntent = Intent(this, LocationTrackingService::class.java)
@@ -511,6 +508,4 @@ private fun NavController.navigateSingleTop(id: Int) {
         val options = builder.build()
         navigate(id, null, options)
     }
-
 }
-
