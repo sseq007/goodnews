11a2b0a703436c90c63e3d6cc38b7b25f6df712d
김예진 <kky156@naver.com>
2023-11-16 14:51:51 +0900

    feat: mypage 서버 연결
    Former-commit-id: 14aca9c5cc35574d1a3adc381569bd070e493c33

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/enterinfo/EnterInfoActivity.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/enterinfo/EnterInfoActivity.kt
@@ -290,7 +290,7 @@ class EnterInfoActivity : AppCompatActivity() {
         // 사용자 입력 값 추출
         val setName = binding.nameEditText.text.toString()
 
-        val setMemberId = userDeviceInfoService.deviceId.toString()+"TEST"
+        val setMemberId = userDeviceInfoService.deviceId.toString()+"save"
         val setPhone = userDeviceInfoService.phoneNumber.toString()
 
         val birthYear = binding.dialogEnterYear.text.toString()
