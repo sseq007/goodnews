9f0ba4f6129321f7fb53eaf49a2e51c0ef06c630
이준용 <elle6044@naver.com>
2023-11-20 22:32:26 +0900

    refactor: bleservice에서 advertise, scan 분리
    Former-commit-id: 7bfbd0123e940a1be174b8e41e08bbd05ff455a6

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/ble/service/BleService.java
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/ble/service/BleService.java
@@ -71,6 +71,7 @@ import com.saveurlife.goodnews.ble.advertise.AdvertiseManager;
 import com.saveurlife.goodnews.ble.message.ChatDatabaseManager;
 //import com.saveurlife.goodnews.ble.message.GroupDatabaseManager;
 import com.saveurlife.goodnews.ble.message.SendMessageManager;
+import com.saveurlife.goodnews.ble.scan.ScanManager;
 import com.saveurlife.goodnews.main.PreferencesUtil;
 import com.saveurlife.goodnews.models.ChatMessage;
 import com.saveurlife.goodnews.service.LocationService;
@@ -95,6 +96,7 @@ import java.util.stream.Collectors;
 
 public class BleService extends Service {
     private AdvertiseManager advertiseManager;
+    private ScanManager scanManager;
 
     private String nowChatRoomID = "";
     private PreferencesUtil preferencesUtil;
@@ -114,8 +116,6 @@ public class BleService extends Service {
     }
 
 
-    private boolean isAdvertising = false;
-
     public static SendMessageManager sendMessageManager;
     private LocationService locationService;
     private UserDeviceInfoService userDeviceInfoService;
@@ -125,7 +125,7 @@ public class BleService extends Service {
     private static String myFamilyId = "";
     private static List<String> myGroupIds = new ArrayList<>();
 
-    private BleServiceScanCallback mBleScanCallback;
+//    private BleServiceScanCallback mBleScanCallback;
     private BluetoothAdapter mBluetoothAdapter;
     private BluetoothLeScanner mBluetoothLeScanner;
     private BluetoothLeAdvertiser mBluetoothLeAdvertiser;
@@ -215,63 +215,14 @@ public class BleService extends Service {
         sendMessageManager = new SendMessageManager(SERVICE_UUID, CHARACTERISTIC_UUID, userDeviceInfoService, locationService, preferencesUtil, myName);
 
         advertiseManager = new AdvertiseManager(mBluetoothAdapter, mBluetoothLeAdvertiser, myId, myName);
+        scanManager = new ScanManager(mBluetoothLeScanner, deviceArrayList, deviceArrayListName, bluetoothDevices, bleMeshConnectedDevicesMap, deviceArrayListNameLiveData);
     }
 
     // 블루투스 시작 버튼
     public void startAdvertiseAndScanAndAuto() {
         advertiseManager.startAdvertising();
-        startScanning();
+        scanManager.startScanning();
         startAutoSendMessage();
-    }
-
-
-
-
-    private void startScanning() {
-        // 현재 실행 중인 스캔이 있는지 확인하고 중지
-        if (mBluetoothLeScanner != null && mBleScanCallback != null) {
-            mBluetoothLeScanner.stopScan(mBleScanCallback);
-        }
-
-        deviceArrayList.clear();
-        deviceArrayListName.clear();
-        bluetoothDevices.clear();
-
-        // 기존 콜백 인스턴스를 재사용하거나 필요한 경우 새 인스턴스를 생성
-        if (mBleScanCallback == null) {
-            mBleScanCallback = new BleServiceScanCallback();
-        }
-
-        ScanSettings settings = new ScanSettings.Builder()
-                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
-                .setLegacy(false)
-                .setPhy(BluetoothDevice.PHY_LE_CODED)
-                .build();
-
-        ScanFilter scanFilter = new ScanFilter.Builder()
-                .setServiceUuid(new ParcelUuid(SERVICE_UUID))
-                .build();
-
-        List<ScanFilter> filters = new ArrayList<>();
-        filters.add(scanFilter);
-
-        mBluetoothLeScanner.startScan(filters, settings, mBleScanCallback);
-
-        scanHandler.post(removeExpiredDevicesRunnable);
-    }
-
-    private void stopScanning() {
-        // BLE 스캐너가 있다면, 스캐닝 중지
-        if (mBluetoothLeScanner != null && mBleScanCallback != null) {
-            mBluetoothLeScanner.stopScan(mBleScanCallback);
-            // 콜백을 null로 설정하여 재사용을 방지
-            mBleScanCallback = null;
-        }
-
-        // Handler를 사용하여 반복 작업을 취소
-        scanHandler.removeCallbacks(removeExpiredDevicesRunnable);
-
-        // 필요한 경우, 여기에 다른 자원 정리 로직을 추가
     }
 
 
@@ -396,94 +347,6 @@ public class BleService extends Service {
 
     public void createChatRoom(String chatRoomId, String chatRoomName) {
 
-    }
-
-
-    private Map<String, Long> lastSeenMap = new HashMap<>();
-    private static final long EXPIRATION_TIME_MS = 4000;
-    private final Handler scanHandler = new Handler(Looper.getMainLooper());
-    private final Runnable removeExpiredDevicesRunnable = new Runnable() {
-        @Override
-        public void run() {
-            removeExpiredDevices();
-            // 예: 10초마다 한 번씩 removeExpiredDevices를 호출합니다.
-            scanHandler.postDelayed(this, 1000);
-        }
-    };
-
-    private void removeExpiredDevices() {
-        long currentTime = System.currentTimeMillis();
-        List<String> devicesToRemove = new ArrayList<>();
-
-        for (Map.Entry<String, Long> entry : lastSeenMap.entrySet()) {
-            if (currentTime - entry.getValue() > EXPIRATION_TIME_MS) {
-                devicesToRemove.add(entry.getKey());
-            }
-        }
-
-        for (String deviceId : devicesToRemove) {
-            int index = deviceArrayList.indexOf(deviceId);
-            if (index != -1) {
-                bluetoothDevices.remove(index);
-                deviceArrayList.remove(index);
-                deviceArrayListName.remove(index);
-            }
-            lastSeenMap.remove(deviceId);
-        }
-
-        if (!devicesToRemove.isEmpty()) {
-            deviceArrayListNameLiveData.postValue(deviceArrayListName);
-        }
-    }
-
-
-    public class BleServiceScanCallback extends ScanCallback {
-        @Override
-        public void onScanResult(int callbackType, ScanResult result) {
-            BluetoothDevice device = result.getDevice();
-            String deviceAddress = device.getAddress();
-
-            // 광고 레코드에서 사용자 이름 데이터 가져오기
-            byte[] userIdBytes = null;
-            byte[] userNameBytes = null;
-            if (result.getScanRecord() != null) {
-                userIdBytes = result.getScanRecord().getServiceData(new ParcelUuid(SERVICE_UUID));
-                userNameBytes = result.getScanRecord().getServiceData(new ParcelUuid(DEVICEINFO_UUID));
-            }
-
-            // 사용자 이름이 null이거나 비어 있을 경우 "Unknown Device"로 표시
-            String deviceId = (userIdBytes != null && userIdBytes.length > 0) ? new String(userIdBytes) : "Unknown Device";
-            String deviceName = (userNameBytes != null && userNameBytes.length > 0) ? new String(userNameBytes) : "Unknown Name";
-
-            for (Map<String, BleMeshConnectedUser> bleMeshConnectedUserMap : bleMeshConnectedDevicesMap.values()) {
-                if (bleMeshConnectedUserMap.containsKey(deviceId)) {
-                    return;
-                }
-            }
-
-            int existingDeviceIndex = -1;
-            for (int i = 0; i < bluetoothDevices.size(); i++) {
-                if (bluetoothDevices.get(i).getAddress().equals(deviceAddress) || deviceArrayList.get(i).equals(deviceId)) {
-                    existingDeviceIndex = i;
-                    break;
-                }
-            }
-
-            // 마지막 감지 시간 업데이트
-            lastSeenMap.put(deviceId, System.currentTimeMillis());
-
-            if (existingDeviceIndex != -1) {
-                bluetoothDevices.set(existingDeviceIndex, device);
-                deviceArrayList.set(existingDeviceIndex, deviceId);
-                deviceArrayListName.set(existingDeviceIndex, deviceId + "/" + deviceName);
-                //                deviceArrayListNameLiveData.postValue(deviceArrayListName); // new code to update LiveData
-            } else {
-                bluetoothDevices.add(device);
-                deviceArrayList.add(deviceId);
-                deviceArrayListName.add(deviceId + "/" + deviceName); // your existing code where you add devices
-                deviceArrayListNameLiveData.postValue(deviceArrayListName); // new code to update LiveData
-            }
-        }
     }
 
 
@@ -958,7 +821,7 @@ public class BleService extends Service {
         super.onDestroy();
         // 광고 중지 로직
         advertiseManager.stopAdvertising();
-        stopScanning();
+        scanManager.stopScanning();
         if (handler != null) {
             handler.removeCallbacksAndMessages(null);
         }
