1640f23e6275d83d90f0ea83e7c6c726dcc53291
선다영 <seondy99@gmail.com>
2023-11-23 12:51:10 +0900

    feat: family 클릭시 데이터 바인딩

--- GoodNews/app/src/main/java/com/saveurlife/goodnews/map/FamilyUserInfoFragment.kt
+++ GoodNews/app/src/main/java/com/saveurlife/goodnews/map/FamilyUserInfoFragment.kt
@@ -7,12 +7,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import com.saveurlife.goodnews.R
 import com.saveurlife.goodnews.databinding.FragmentFamilyUserInfoBinding
 
 class FamilyUserInfoFragment : DialogFragment() {
     private lateinit var binding: FragmentFamilyUserInfoBinding
+
+    private lateinit var userName: String
+    private lateinit var userStatus: String
+    private lateinit var userUpdateTime: String
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +27,37 @@ class FamilyUserInfoFragment : DialogFragment() {
         dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         
         // 데이터 받아와 화면에 표시
-        arguments?.let { bundle ->  }
+        arguments?.let { bundle ->
+            userName = bundle.getString("userName").toString()
+            userStatus = bundle.getString("userStatus").toString()
+            userUpdateTime = bundle.getString("userUpdateTime").toString()
+
+            binding.familyNameTextView.text = userName
+            binding.familyLastTextView.text = userUpdateTime
+
+            // 상태에 따라 상태 ui 색상 변경
+            when (userStatus) {
+                "safe" -> context?.let { ctx ->
+                    this.binding.familyStatusCircle.backgroundTintList =
+                        ContextCompat.getColorStateList(ctx, R.color.safe)
+                }
+
+
+                "injure" -> context?.let { ctx ->
+                    this.binding.familyStatusCircle.backgroundTintList =
+                        ContextCompat.getColorStateList(ctx, R.color.caution)
+                }
+
+                "death" -> context?.let { ctx ->
+                    this.binding.familyStatusCircle.backgroundTintList =
+                        ContextCompat.getColorStateList(ctx, R.color.black)
+                }
+
+                else -> {
+
+                }
+            }
+        }
 
         // 닫기 버튼 클릭했을 때
         binding.familyCloseButton.setOnClickListener {
